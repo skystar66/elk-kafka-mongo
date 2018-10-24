8ae4f792fa60f513ca8917a48dc0c74fc9cb654e
xu.liang <xu.liang@xcar.com.cn>
2018-10-24 11:53:17 +0800

    完善kafka

--- src/main/java/com/xcar360/action/Request1001Handler.java
+++ src/main/java/com/xcar360/action/Request1001Handler.java
@@ -1,19 +1,15 @@
 package com.xcar360.action;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
 import com.xcar360.kafka.config.KafkaSendResultHandler;
 import com.xcar360.mongo.MessageTemplate;
 import com.xcar360.util.KafkaConstants;
 import com.xcar360.util.ReturnCode;
 import com.xcar360.web.auth.AbstractRequest;
-import com.xcar360.web.response.ResponseResult;
-import org.apache.kafka.clients.producer.ProducerRecord;
-import org.slf4j.Logger;
+import com.xcar360.web.response.ResponseResult;import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
